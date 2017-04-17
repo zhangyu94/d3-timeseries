624156d746f44981ea69faaf1f7eb6a67c1b1cbf
zhangyu94 <463568697@qq.com>
2017-04-17 08:38:16 +0800

    grammar

--- libs/d3_timeseries/d3.horizon.js
+++ libs/d3_timeseries/d3.horizon.js
@@ -1,4 +1,4 @@
-//version 2017.3.26 12:00
+//version 2017.4.8 12:00
 //dependency:
 //d3.js version 3.5.17
 
@@ -17,13 +17,13 @@
                 bottom: 20,
                 left: 20
             },
-            duration = 0;
+            duration = 0
 
-        let opacity = 1;
+        let opacity = 1
         let color = d3.scale.linear()
             .domain([-1, 0, 0, 1])
-            .range(["#08519c", "#bdd7e7", "#bae4b3", "#006d2c"]);
-        let global_value_extent = undefined;
+            .range(["#08519c", "#bdd7e7", "#bae4b3", "#006d2c"])
+        let global_value_extent = undefined
 
         //renderer
         function chart(selection) {
@@ -33,7 +33,7 @@
             //ele[0]是x轴, ele[1]是y轴
             selection.each(function(d, i) {
                 let innerWidth = width - margin.left - margin.right,
-                    innerHeight = height - margin.top - margin.bottom;
+                    innerHeight = height - margin.top - margin.bottom
 
                 let svg = d3.select(this)
                     .attr("width", width)
@@ -51,13 +51,13 @@
                 //data就等于d
                 let data = d.map(function(d, i) {
                     let xv = x.call(this, d, i),
-                        yv = y.call(this, d, i);
-                    if (xv < xMin) xMin = xv;
-                    if (xv > xMax) xMax = xv;
-                    if (-yv > yMax) yMax = -yv;
-                    if (yv > yMax) yMax = yv;
-                    return [xv, yv];
-                });
+                        yv = y.call(this, d, i)
+                    if (xv < xMin) xMin = xv
+                    if (xv > xMax) xMax = xv
+                    if (-yv > yMax) yMax = -yv
+                    if (yv > yMax) yMax = yv
+                    return [xv, yv]
+                })
 
                 //如果传入了global_value_extent用于统一位置和颜色映射的基准,用这个基准来修改yMax
                 if (global_value_extent != undefined)
@@ -68,86 +68,86 @@
                 // Compute the new x- and y-scales, and transform.
                 let x1 = d3.scale.linear().domain([xMin, xMax]).range([0, innerWidth]),
                     y1 = d3.scale.linear().domain([0, yMax]).range([0, innerHeight * bands]),
-                    t1 = d3_horizonTransform(bands, innerHeight, mode);
+                    t1 = d3_horizonTransform(bands, innerHeight, mode)
 
                 // Retrieve the old scales, if this is an update.
                 if (this.__horizon__) {
-                    x0 = this.__horizon__.x;
-                    y0 = this.__horizon__.y;
-                    t0 = this.__horizon__.t;
-                    id = this.__horizon__.id;
+                    x0 = this.__horizon__.x
+                    y0 = this.__horizon__.y
+                    t0 = this.__horizon__.t
+                    id = this.__horizon__.id
                 } else {
-                    x0 = x1.copy();
-                    y0 = y1.copy();
-                    t0 = t1;
-                    id = ++d3_horizonId;
+                    x0 = x1.copy()
+                    y0 = y1.copy()
+                    t0 = t1
+                    id = ++d3_horizonId
                 }
 
                 // We'll use a defs to store the area path and the clip path.
                 let defs = svg.selectAll("defs")
-                    .data([null]);
+                    .data([null])
 
                 // The clip path is a simple rect.
                 defs.enter().append("defs").append("clipPath")
                     .attr("id", "d3_horizon_clip" + id)
                     .append("rect")
                     .attr("width", innerWidth)
-                    .attr("height", innerHeight);
+                    .attr("height", innerHeight)
 
                 defs.select("rect").transition()
                     .duration(duration)
                     .attr("width", innerWidth)
-                    .attr("height", innerHeight);
+                    .attr("height", innerHeight)
 
                 // We'll use a container to clip all horizon layers at once.
                 svg.selectAll("g")
                     .data([null])
                     .enter().append("g")
                     .attr("clip-path", "url(#d3_horizon_clip" + id + ")")
-                    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+                    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
 
                 // Instantiate each copy of the path with different transforms.
                 let path = svg.select("g").selectAll("path")
-                    .data(d3.range(-1, -bands - 1, -1).concat(d3.range(1, bands + 1)), Number);
+                    .data(d3.range(-1, -bands - 1, -1).concat(d3.range(1, bands + 1)), Number)
 
                 let d0 = d3_horizonArea
                     .interpolate(interpolate)
                     .x(function(d) {
-                        return x0(d[0]);
+                        return x0(d[0])
                     })
                     .y0(innerHeight * bands)
                     .y1(function(d) {
-                        return innerHeight * bands - y0(d[1]);
+                        return innerHeight * bands - y0(d[1])
                     })
-                    (data);
+                    (data)
 
                 let d1 = d3_horizonArea
                     .x(function(d) {
-                        return x1(d[0]);
+                        return x1(d[0])
                     })
                     .y1(function(d) {
-                        return innerHeight * bands - y1(d[1]);
+                        return innerHeight * bands - y1(d[1])
                     })
-                    (data);
+                    (data)
 
                 path.enter().append("path")
                     .attr('opacity', opacity)
                     .style("fill", color)
                     .attr("transform", t0)
-                    .attr("d", d0);
+                    .attr("d", d0)
 
                 path.transition()
                     .duration(duration)
                     .attr('opacity', opacity)
                     .style("fill", color)
                     .attr("transform", t1)
-                    .attr("d", d1);
+                    .attr("d", d1)
 
                 path.exit().transition()
                     .duration(duration)
                     .attr("transform", t1)
                     .attr("d", d1)
-                    .remove();
+                    .remove()
 
                 // Stash the new scales.
                 this.__horizon__ = {
@@ -155,119 +155,119 @@
                     y: y1,
                     t: t1,
                     id: id
-                };
-            });
-            d3.timer.flush();
+                }
+            })
+            d3.timer.flush()
         }
 
         //setter & getter
         chart.margin = function(value) {
-            if (!arguments.length) return margin;
+            if (!arguments.length) return margin
             if (typeof(value) != "object") {
-                console.warn("invalid value for margin", value);
-                return;
+                console.warn("invalid value for margin", value)
+                return
             }
             if (typeof(value.top) == "number")
-                margin.top = value.top;
+                margin.top = value.top
             if (typeof(value.right) == "number")
-                margin.right = value.right;
+                margin.right = value.right
             if (typeof(value.bottom) == "number")
-                margin.bottom = value.bottom;
+                margin.bottom = value.bottom
             if (typeof(value.left) == "number")
-                margin.left = value.left;
-            return chart;
-        };
+                margin.left = value.left
+            return chart
+        }
 
         chart.duration = function(value) {
-            if (!arguments.length) return duration;
-            duration = +value;
-            return chart;
-        };
+            if (!arguments.length) return duration
+            duration = +value
+            return chart
+        }
 
         chart.bands = function(value) {
-            if (!arguments.length) return bands;
-            bands = +value;
-            color.domain([-bands, 0, 0, bands]);
-            return chart;
-        };
+            if (!arguments.length) return bands
+            bands = +value
+            color.domain([-bands, 0, 0, bands])
+            return chart
+        }
 
         chart.mode = function(value) {
-            if (!arguments.length) return mode;
-            mode = value + "";
-            return chart;
-        };
+            if (!arguments.length) return mode
+            mode = value + ""
+            return chart
+        }
 
         chart.opacity = function(value) {
-            if (!arguments.length) return opacity;
-            opacity = value;
-            return chart;
-        };
+            if (!arguments.length) return opacity
+            opacity = value
+            return chart
+        }
 
         chart.colors = function(value) {
-            if (!arguments.length) return color.range();
-            color.range(value);
-            return chart;
-        };
+            if (!arguments.length) return color.range()
+            color.range(value)
+            return chart
+        }
 
         chart.interpolate = function(value) {
-            if (!arguments.length) return interpolate;
-            interpolate = value + "";
-            return chart;
-        };
+            if (!arguments.length) return interpolate
+            interpolate = value + ""
+            return chart
+        }
 
         chart.x = function(value) {
-            if (!arguments.length) return x;
-            x = value;
-            return chart;
-        };
+            if (!arguments.length) return x
+            x = value
+            return chart
+        }
 
         chart.y = function(value) {
-            if (!arguments.length) return y;
-            y = value;
-            return chart;
-        };
+            if (!arguments.length) return y
+            y = value
+            return chart
+        }
 
         chart.width = function(value) {
-            if (!arguments.length) return width;
-            width = +value;
-            return chart;
-        };
+            if (!arguments.length) return width
+            width = +value
+            return chart
+        }
 
         chart.height = function(value) {
-            if (!arguments.length) return height;
-            height = +value;
-            return chart;
-        };
+            if (!arguments.length) return height
+            height = +value
+            return chart
+        }
 
         chart.global_value_extent = function(value) {
-            if (!arguments.length) return global_value_extent;
+            if (!arguments.length) return global_value_extent
             if (typeof(value) != "object") {
-                console.warn("invalid value for global_value_extent", value);
-                return;
+                console.warn("invalid value for global_value_extent", value)
+                return
             }
-            global_value_extent = value;
-            return chart;
-        };
+            global_value_extent = value
+            return chart
+        }
 
-        return chart;
-    };
+        return chart
+    }
 
     let d3_horizonArea = d3.svg.area(),
-        d3_horizonId = 0;
+        d3_horizonId = 0
 
     function d3_horizonX(d) {
-        return d[0];
+        return d[0]
     }
 
     function d3_horizonY(d) {
-        return d[1];
+        return d[1]
     }
 
     function d3_horizonTransform(bands, height, mode) {
         return mode == "offset" ? function(d) {
-            return "translate(0," + (d + (d < 0) - bands) * height + ")";
+            return "translate(0," + (d + (d < 0) - bands) * height + ")"
         } : function(d) {
-            return (d < 0 ? "scale(1,-1)" : "") + "translate(0," + (d - bands) * height + ")";
-        };
+            return (d < 0 ? "scale(1,-1)" : "") + "translate(0," + (d - bands) * height + ")"
+        }
     }
 })();
--- libs/d3_timeseries/d3.multiresolution.js
+++ libs/d3_timeseries/d3.multiresolution.js
@@ -1,4 +1,4 @@
-//version 2017.3.26 12:00
+//version 2017.4.8 12:00
 //dependency:
 //d3.js version 3.5.17
 //jquery.js version 2.1.1
@@ -19,60 +19,60 @@
             //[-无穷, 5)对应resolution0, [5,20)对应resolution1, [20, 无穷]对应resolution2
             enable_transition = false, //启用同视图transition时渲染速度会严重减慢,比删掉以后重新渲染还慢
             thersholds = d3.scale.threshold().domain([5, 20]).range([0, 1, 2]),
-            global_value_extent = undefined;
+            global_value_extent = undefined
 
         //local function & variable
-        let _previous_resolution = undefined;
+        let _previous_resolution = undefined
 
         function _get_horizon_data(heavy_line) {
-            let light_linechart = [];
+            let light_linechart = []
             for (let i = 0; i < heavy_line.data.length; ++i) {
-                let x = heavy_line.data[i].x;
-                let y = heavy_line.data[i].y;
+                let x = heavy_line.data[i].x
+                let y = heavy_line.data[i].y
                 light_linechart.push([x, y])
             }
-            return light_linechart;
+            return light_linechart
         }
 
         function _get_bitmap_data(heavy_line) {
             let matrix = [
                 []
-            ];
+            ]
             for (let i = 0; i < heavy_line.data.length; ++i) {
                 //此处假设heavy_line.data[...].x都是等间距的
                 matrix[0][i] = {
                     val: heavy_line.data[i].y,
                 }
             }
-            return matrix;
+            return matrix
         }
 
         function _get_light_data(heavy_line) {
             let transformed_data = {
                 x: [],
                 y: [],
-            };
+            }
             for (let i = 0; i < heavy_line.data.length; ++i) {
-                transformed_data.x.push(heavy_line.data[i].x);
+                transformed_data.x.push(heavy_line.data[i].x)
                 transformed_data.y.push(heavy_line.data[i].y)
             }
-            return transformed_data;
+            return transformed_data
         }
 
         //renderer
         function chart(selection) {
             selection.each(function(dataset_line) {
-                let innerHeight = height - margin.top - margin.bottom;
+                let innerHeight = height - margin.top - margin.bottom
 
-                let div = d3.select(this);
-                let resolution = thersholds(innerHeight);
+                let div = d3.select(this)
+                let resolution = thersholds(innerHeight)
 
                 function _handle_renderplace(div, resolution, _previous_resolution, enable_transition) {
                     if ((resolution == _previous_resolution) && enable_transition)
-                        return;
+                        return
 
-                    div.selectAll('canvas').remove();
-                    div.selectAll('svg').remove();
+                    div.selectAll('canvas').remove()
+                    div.selectAll('svg').remove()
                     if (resolution == 0) {
                         div.append("canvas")
                             .style('display', 'block')
@@ -87,10 +87,10 @@
                     }
 
                 }
-                _handle_renderplace(div, resolution, _previous_resolution, enable_transition);
+                _handle_renderplace(div, resolution, _previous_resolution, enable_transition)
                 if (resolution == 0) //render bitmap
                 {
-                    let bitmap_data = _get_bitmap_data(dataset_line);
+                    let bitmap_data = _get_bitmap_data(dataset_line)
 
                     let bitmap = d3.bitmap()
                         .width(width)
@@ -102,7 +102,7 @@
                         bitmap.global_value_extent(global_value_extent)
 
                     let canvas = div.select("canvas")
-                    canvas.data([bitmap_data]).call(bitmap.duration(duration));
+                    canvas.data([bitmap_data]).call(bitmap.duration(duration))
                 } else if (resolution == 1) //render horizon_graph
                 {
                     let horizon_data = _get_horizon_data(dataset_line)
@@ -124,10 +124,10 @@
                     }
 
                     let svg = div.select("svg")
-                    svg.data([normalized_horizon_data]).call(horizon.duration(duration));
+                    svg.data([normalized_horizon_data]).call(horizon.duration(duration))
                 } else if (resolution == 2) //render light_linechart
                 {
-                    let transformed_data = _get_light_data(dataset_line);
+                    let transformed_data = _get_light_data(dataset_line)
 
                     let linechart = d3.linechart_light()
                         .width(width)
@@ -139,102 +139,102 @@
                     let svg = div.select("svg")
                     svg.data([
                         [transformed_data]
-                    ]).call(linechart.duration(duration));
+                    ]).call(linechart.duration(duration))
                 }
 
-                _previous_resolution = resolution;
+                _previous_resolution = resolution
 
-            });
+            })
         }
 
         //setter & getter
         chart.width = function(value) {
-            if (!arguments.length) return width;
-            width = value;
-            return chart;
-        };
+            if (!arguments.length) return width
+            width = value
+            return chart
+        }
 
         chart.height = function(value) {
-            if (!arguments.length) return height;
-            height = value;
-            return chart;
-        };
+            if (!arguments.length) return height
+            height = value
+            return chart
+        }
 
         chart.margin = function(value) {
-            if (!arguments.length) return margin;
+            if (!arguments.length) return margin
             if (typeof(value) != "object") {
-                console.warn("invalid value for margin", value);
-                return;
+                console.warn("invalid value for margin", value)
+                return
             }
             if (typeof(value.top) == "number")
-                margin.top = value.top;
+                margin.top = value.top
             if (typeof(value.right) == "number")
-                margin.right = value.right;
+                margin.right = value.right
             if (typeof(value.bottom) == "number")
-                margin.bottom = value.bottom;
+                margin.bottom = value.bottom
             if (typeof(value.left) == "number")
-                margin.left = value.left;
-            return chart;
-        };
+                margin.left = value.left
+            return chart
+        }
 
         chart.duration = function(value) {
-            if (!arguments.length) return duration;
+            if (!arguments.length) return duration
             if (typeof(value) != "number") {
-                console.warn("invalid value for duration", value);
-                return;
+                console.warn("invalid value for duration", value)
+                return
             }
-            duration = value;
-            return chart;
-        };
+            duration = value
+            return chart
+        }
 
         chart.opacity = function(value) {
-            if (!arguments.length) return opacity;
+            if (!arguments.length) return opacity
             if (typeof(value) != "number") {
-                console.warn("invalid value for opacity", value);
-                return;
+                console.warn("invalid value for opacity", value)
+                return
             }
-            opacity = value;
-            return chart;
-        };
+            opacity = value
+            return chart
+        }
 
         chart.color_scale = function(value) {
-            if (!arguments.length) return color_scale;
+            if (!arguments.length) return color_scale
             if (typeof(value) != "function") {
-                console.warn("invalid value for color_scale", value);
-                return;
+                console.warn("invalid value for color_scale", value)
+                return
             }
-            color_scale = value;
-            return chart;
-        };
+            color_scale = value
+            return chart
+        }
 
         chart.enable_transition = function(value) {
-            if (!arguments.length) return enable_transition;
+            if (!arguments.length) return enable_transition
             if (typeof(value) != "boolean") {
-                console.warn("invalid value for enable_transition", value);
-                return;
+                console.warn("invalid value for enable_transition", value)
+                return
             }
-            enable_transition = value;
-            return chart;
-        };
+            enable_transition = value
+            return chart
+        }
 
         chart.global_value_extent = function(value) {
-            if (!arguments.length) return global_value_extent;
+            if (!arguments.length) return global_value_extent
             if (typeof(value) != "object") {
-                console.warn("invalid value for global_value_extent", value);
-                return;
+                console.warn("invalid value for global_value_extent", value)
+                return
             }
-            global_value_extent = value;
-            return chart;
-        };
+            global_value_extent = value
+            return chart
+        }
 
-        return chart;
+        return chart
     }
-})();
+})()
 
 $.fn.d3_multiresolution = function() {
     if ($(this).data('d3_multiresolution') == undefined) {
-        let this_width = this.width();
-        let this_height = this.height();
+        let this_width = this.width()
+        let this_height = this.height()
         let renderer = d3.multiresolution()
             .width(this_width)
             .height(this_height)
@@ -246,5 +246,5 @@ $.fn.d3_multiresolution = function() {
             })
         $(this).data('d3_multiresolution', renderer)
     }
-    return $(this).data('d3_multiresolution');
+    return $(this).data('d3_multiresolution')
 }
